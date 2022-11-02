13c862fdc4fb8643f4bea191aafe9a509c2180d5
Venislav Kirilov <60657151+vkirilov1@users.noreply.github.com>
2022-11-02 20:38:30 +0200

    Delete circle_area.py

--- circle_area.py
+++
@@ -1,8 +0,0 @@
-import math
-
-r = float(input('Enter radius: '))
-perimeter = 2 * math.pi * r
-area = math.pi * r * r
-
-print('The perimeter of the circle is: ', round(perimeter, 3))
-print('The area of the circle is: ', round(area, 3))
