3e10e8ab86f0474989fadb4fe0ce2399e1680e9d
Venislav Kirilov <60657151+vkirilov1@users.noreply.github.com>
2022-11-02 20:38:52 +0200

    Delete trapezoid_area.py

--- trapezoid_area.py
+++
@@ -1,6 +0,0 @@
-a = float(input('Enter first side: '))
-b = float(input('Enter second side: '))
-h = float(input('Enter height: '))
-
-trapezoid_area = (a + b) * h / 2
-print('The area of the trapezoid is:', round(trapezoid_area,2))
