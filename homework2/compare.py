628968997525b390c59b4f20560b3048294498e5
Venislav Kirilov <60657151+vkirilov1@users.noreply.github.com>
2022-11-03 18:09:33 +0200

    Delete homework2 directory

--- homework2/compare.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter your number: "))
-bit = int(input("Enter your bit: "))
-
-if number & (1 << bit) > 0:
-    print("False")
-else:
-     print("True")
--- homework2/money&hours.py
+++
@@ -1,5 +0,0 @@
-hours = float(input('Enter hours count: '))
-rate = float(input('Enter rate: '))
-
-money = hours * rate
-print('Money to pay: ', money)
