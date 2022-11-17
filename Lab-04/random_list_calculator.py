90ce9900d7518524aff8d1d2b90e147afad73001
Venislav Kirilov <vkirilov420@abv.bg>
2022-11-17 10:13:19 +0200

    update

--- Lab-04/random_list_calculator.py
+++ Lab-04/random_list_calculator.py
@@ -3,23 +3,16 @@ import random
 count = int(input("Input the size of the list: "))
 
 numbers = []
-summed_numbers = []
+result = []
 
 for i in range(count):
     numbers.append(random.randint(1, 99))
 
-for n in range(0, len(numbers)):
-    if n+1 == len(numbers):
-        break
+for n in range(0, count-1):
     new_element = numbers[n] + numbers[n+1]
-    summed_numbers.append(new_element)
-
-result = []
-for a in range(0, len(numbers)):
-    result.append(numbers[a])
-    if a == len(summed_numbers):
-        break
-    result.append(summed_numbers[a])
+    result.append(numbers[n])
+    result.append(new_element)
+result.append(numbers[-1])
 
 print("Random list: " + str(numbers))
 print("List with summed numbers: " + str(result))
